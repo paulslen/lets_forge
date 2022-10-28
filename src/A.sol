f45a1be1eef0ce7926dd3d4bee5bdb62f3143162
Paul Len <paulstanlen@gmail.com>
2022-10-28 12:41:37 -0400

    Contract A from Lesson 8

---
+++ src/A.sol
@@ -0,0 +1,17 @@
+contract A {
+ uint256 number;
+ /**
+ * @dev Store value in variable
+ * @param num value to store
+ */
+ function store(uint256 num) public {
+ number = num;
+ }
+ /**
+ * @dev Return value
+ * @return value of 'number'
+ */
+ function retrieve() public view returns (uint256) {
+ return number;
+ }
+}
